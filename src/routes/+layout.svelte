6b2777ad539fbee7f91b18bdf5c9babaeefeb86f
triesap <137732411+triesap@users.noreply.github.com>
2023-07-30 18:26:48 +0000

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<title>Seed Sanctuary</title>
+	<meta name="description" content="A seed exchange for farmers that uses Nostr and WebLN" />
+</svelte:head>
+<slot />
