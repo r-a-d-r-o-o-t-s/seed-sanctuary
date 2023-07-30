5b28c501f30fe1eec93b675889aa1b27f9d798e6
triesap <137732411+triesap@users.noreply.github.com>
2023-07-30 19:24:27 +0000

    Add nostr event kind 30402 subscription

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,33 @@
-<p>seed sanctuary</p>
+<script lang="ts">
+	import { ndk } from '$lib/store/ndk';
+	import type { NDKEvent } from '@nostr-dev-kit/ndk';
+
+	let subscriptionEvents: NDKEvent[] = [];
+
+	const kinds: number[] = [30402];
+	const subscription = $ndk.subscribe({ kinds }, { closeOnEose: false });
+
+	subscription.on(`event`, (event) => {
+		subscriptionEvents.push(event);
+	});
+</script>
+
+<div class="flex flex-col w-full">
+	{#if subscriptionEvents.length}
+		{#each subscriptionEvents as se}
+			{#key se}
+				<div class="flex flex-row w-full">
+					<p class="font-sans font-medium text-base">
+						{`Found seeds posted by ${se.author}`}
+					</p>
+				</div>
+			{/key}
+		{/each}
+	{:else}
+		<div class="flex flex-row">
+			<p class="font-sans font-medium text-base">
+				{`There are currently no seeds posted to the exchange`}
+			</p>
+		</div>
+	{/if}
+</div>
